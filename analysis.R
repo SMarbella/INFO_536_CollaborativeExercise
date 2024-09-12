28eba0b8d89680ff5a6487f861f28b691172e335
SMarbella <swmarbella@arizona.edu>
2024-09-12 15:18:43 -0700

    Cleaned the data.

--- analysis.R
+++ analysis.R
@@ -13,6 +13,24 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
 # Basic data exploration
 glimpse(gtd_data)
 
+# Data cleaning
+# Counting NA values
+sum(is.na(gtd_data$targtype1)) # 0 NAs
+sum(is.na(gtd_data$targtype1_txt)) # 0 NAs
+
+sum(is.na(gtd_data$targtype2)) # 170547 NAs
+sum(is.na(gtd_data$targtype2_txt)) # 0 NAs
+
+sum(is.na(gtd_data$targtype3)) # 0180515 NAs
+sum(is.na(gtd_data$targtype3_txt)) # 0 NAs
+
+# Remove NAs
+gtd_clean <- gtd_data %>%
+  select(targtype1_txt, targtype2_txt, targtype3_txt, targtype1, targtype2, targtype3) %>%
+  filter(!is.na(targtype1), !is.na(targtype2), !is.na(targtype3))
+
+glimpse(gtd_clean)
+
 # Role 1: Filter and group data by target type.
 
 
